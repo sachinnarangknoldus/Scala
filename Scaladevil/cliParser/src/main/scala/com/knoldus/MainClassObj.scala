a08cd9e5b84e56ecd6c1473ebffc5752188bf569
sachinnarangknoldus <76532103+sachinnarangknoldus@users.noreply.github.com>
2021-03-07 16:49:46 +0530

    Delete Scaladevil directory

--- Scaladevil/cliParser/src/main/scala/com/knoldus/MainClassObj.scala
+++
@@ -1,9 +0,0 @@
-package com.knoldus
-
-object MainClassObj {
-    def main(args: Array[String]): Unit = {
-        val list = args.toList
-        val obj = new Cliparser
-        obj.driver_Func(list)
-    }
-}
